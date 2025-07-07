328770ecb89e10f6196d4e45e2ffd2663982eace
Bruno Furlan <furlanbruno46@gmail.com>
2025-07-07 20:32:57 -0300

    Add files via upload

---
+++ função.js
@@ -0,0 +1,3 @@
+function mostrarMensagem() {
+  document.getElementById('mensagem').innerText = "Jamais considere seus estu-dos como uma obrigação, mas como uma oportunidade invejável para aprender a conhecer a beleza libertadora do intelecto para seu próprio prazer pessoal e para proveito da comunidade à qual seu futuro trabalho pertencer.(Albert Einstein)";
+}
